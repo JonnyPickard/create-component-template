9dff0682077667e2b4b4abd283a5b4ea5e121267
Jonny Pickard <jonathandpickard@gmail.com>
2018-07-07 20:53:02 +0100

    Pathing test 1

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -1,17 +1,23 @@
 // @flow
 
+console.log(`process.env`, process.env.NODE_ENV);
+
+console.log(__dirname);
+
 const path = require('path');
 const pkgDir = require('pkg-dir');
 
 const packageDir = pkgDir.sync(__dirname);
 const splitPath = packageDir.split(path.sep);
 
+console.log(`packageDir`, packageDir);
+
 /**
  * gets the root dir based on parent dir
  * Checks to see whether being run from node_modules or locally
  * @return {string} rootDir path
  */
-const getRootDir = () => {
+const getModuleRootDir = () => {
   let rootDir = isNodeModule()
     ? packageDir
         .split(path.sep)
@@ -32,6 +38,6 @@ const isNodeModule = () => {
 
 module.exports = {
   capitalize: (string: string) => string.replace(/\b\w/g, l => l.toUpperCase()),
-  getRootDir,
+  getRootDir: getModuleRootDir,
   isNodeModule
 };
